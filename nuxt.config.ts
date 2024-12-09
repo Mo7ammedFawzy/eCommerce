19092988761998d7e9ae5d30b04647c1746427de
devfawzey <devfawzey@gmail.com>
2024-12-09 15:24:41 +0200

    add_image_vercel_provider

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
     }
   },
   image: {
-    // provider:"vercel"
+    provider:"vercel"
   },
   icon: {
     customCollections: [
@@ -41,5 +41,6 @@ export default defineNuxtConfig({
   notivue: {
     limit: 3,
     avoidDuplicates: true,
-  }
+  },
+  
 })
