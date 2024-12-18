a841f88bf73d54b8bf2f5dc5baf545b15835135a
devfawzey <devfawzey@gmail.com>
2024-12-18 21:56:38 +0200

    add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app:{
+    head:{
+      title:"eCommerceHope"
+    }
+  },
   devtools: {
     enabled: true,
 
