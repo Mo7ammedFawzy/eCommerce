232b56a65f560784d3161c12b48f298f12e4fb08
Mohammed Fawzey <139973567+devfawzey@users.noreply.github.com>
2025-07-03 18:05:20 +0300

    Squashed 'frontend/vue/' content from commit 07e2a3d
    git-subtree-dir: frontend/vue
    git-subtree-split: 07e2a3d32004e6b624a3d0a5c2c90adb46cf219e

---
+++ src/views/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <UWrapper class="flex items-center justify-center min-h-screen w-full">
+    <div class="p-4 flex justify-center border border-(--ui-border-muted) rounded-md max-w-fit">
+      HOME PAGE
+    </div>
+  </UWrapper>
+</template>
