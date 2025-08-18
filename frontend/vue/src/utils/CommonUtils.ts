f9972afe973b5caceb013de2919ddadc907924d9
Mohammed Fawzey <139973567+devfawzey@users.noreply.github.com>
2025-08-18 11:04:58 +0300

    [UI] Fix images path

--- frontend/vue/src/utils/CommonUtils.ts
+++ frontend/vue/src/utils/CommonUtils.ts
@@ -4,8 +4,7 @@ import {MaybeRefOrGetter} from "@vueuse/core";
 
 export default class CommonUtils {
   static getImgUrl(imgPath: string) {
-
-    const url = `../assets/imgs${imgPath}`;
+    const url = `/src/assets/imgs${imgPath}`;
     const base = import.meta.url;
     return new URL(url, base).href
   }
