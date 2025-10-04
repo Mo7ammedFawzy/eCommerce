fb55ed263fef12f375cfb7e92236a575fa49950b
Mo7ammedFawzy <devfawzey@gmail.com>
2025-10-04 18:09:33 +0300

    [UI] Fix

--- frontend/vue/src/views/CheckoutPage.vue
+++ frontend/vue/src/views/CheckoutPage.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-
 import ComingSoon from "@/components/ComingSoon.vue";
 </script>
-
 <template>
   <ComingSoon/>
 </template>
--- frontend/vue/src/views/profile/ProfilePage.vue
+++ frontend/vue/src/views/profile/ProfilePage.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
-
+import ComingSoon from "@/components/ComingSoon.vue";
 </script>
-
 <template>
   <ComingSoon/>
 </template>
-
-<style scoped>
-
-</style>
