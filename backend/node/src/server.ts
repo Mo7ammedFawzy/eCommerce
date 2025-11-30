9c7b194aadea7156be1dcb4e53c530e32f94d234
Mo7ammedFawzy <devfawzey@gmail.com>
2025-11-30 08:55:42 +0200

    [Deploy]

--- backend/node/src/server.ts
+++ backend/node/src/server.ts
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app";
 import {connectDB} from "./config/db";
+import {VercelRequest, VercelResponse} from "@vercel/node";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const isDevMode = process.env.NODE_ENV === "development";
 
-(async () => {
+async function start() {
   await connectDB()
   app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
   });
-})();
+}
+
+if (isDevMode)
+  start();
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return app(req, res);
+}
