8e053a34a0d7676fead32abf07b69855ddc943fc
Mo7ammedFawzy <devfawzey@gmail.com>
2025-11-30 09:23:07 +0200

    [UI] Reload product after id changed

--- frontend/vue/src/views/products/ProductPage.vue
+++ frontend/vue/src/views/products/ProductPage.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import {getProduct} from "@/composables/useApi.ts";
 import SkeletonProductContent from "@/components/product/SkeletonProductContent.vue";
+import {computed} from "vue";
 
 const props = defineProps<{ id: string, title: string }>()
 
-const {data} = getProduct(props.id);
+const {data, isFetching} = getProduct(computed(() => props.id));
 </script>
 
 <template>
-  <BaseWrapper class="my-6 relative" :key="id">
+  <BaseWrapper class="my-6 relative">
     <BaseLandingImg>
       <BaseBlob/>
     </BaseLandingImg>
     <BasePageHeader :label="title"/>
-    <ProductContent v-bind="data?.data.product" v-if="data?.data.product"/>
+    <ProductContent v-bind="data?.data.product" v-if="data?.data.product &&!isFetching"/>
     <SkeletonProductContent v-else/>
   </BaseWrapper>
 </template>
